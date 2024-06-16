0ba760ce8f5ca87c1a186f31948a5a785cad9dbf
aymanfad <97688907+aymanfad@users.noreply.github.com>
2024-06-16 14:52:10 +0200

    first commit

---
+++ src/test/java/com/exemple/reservationsystem/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.reservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
